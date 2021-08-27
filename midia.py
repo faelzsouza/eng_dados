2ed0d166478134f260a100ae80cd25031fcd4fd3
Rafael Souza <faelzsouza@gmail.com>
2021-08-26 23:38:53 -0300

    todo CRUD + sample Netflix

--- midia.py
+++ midia.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+import pygame
+from time import sleep
 
 class Midia(ABC):
     def __init__(self, tipo, id):
@@ -15,6 +17,10 @@ class Midia(ABC):
 
     @abstractmethod
     def deletar():
+        pass
+
+    @abstractmethod
+    def buscaTudo():
         pass
 
 class Filme(Midia):
@@ -48,20 +54,80 @@ class Serie(Filme):
         super().__init__(tipo, id, titulo, direcao, ano, genero, sinopse)
         self.episodios = episodios
         self.produtora = produtora
-    
+
     def armazenar(self, obj):
-        pass
+        listaSeries.append(vars(obj))
 
-    def assistir(self):
-        pass
+    def assistir(self, index):
+        print(f'Você está assistindo t01ep01 de {listaSeries[index]["titulo"]}...')
+        pygame.init()
+        pygame.mixer.music.load('nouveau-jingle-netflix.mp3')
+        pygame.mixer.music.play()
+        pygame.event.wait()
+        sleep(5)
 
-    def atualizar(self, id):
-        pass
+    def atualizar(self, index, chave, valor):
+        listaSeries[index][chave] = valor
 
-    def deletar(self, id):
-        pass
+    def deletar(self, index):
+        listaSeries.pop(index)
+    
+    def buscaTudo(self):
+        for i, s in enumerate(listaSeries):
+            print(f'[{i+1}] {listaSeries[i]["titulo"]}') # Mostra as séries que estão na lista!
 
 listaSeries = []
+id = 0
+while True:
+    print('''[1] Adicionar Série
+[2] Assistir Série
+[3] Atualizar Série
+[4] Deletar Série
+[0] Sair do Programa''')
+    r = int(input())
+    if r == 1:
+        print('Titulo: ')
+        titulo = input()
+        if titulo == '0':
+            break
+        print('Direção: ')
+        direcao = input()
+        print('Ano: ')
+        ano = int(input())
+        print('Genero: ')
+        genero = input()
+        print('Sinopse: ')
+        sinopse = input()
+        print('Episódios: ')
+        ep = int(input())
+        print('Produtora: ')
+        prod = input()
+        serie = Serie('visual', id, titulo, direcao, ano, genero, sinopse, ep, prod)
+        serie.armazenar(serie)
+        id += 1
+    elif r == 2:
+        print('Qual série deseja assistir?')
+        serie.buscaTudo()
+        serie.assistir(int(input()) - 1)
+    elif r == 3:
+        print('Qual série deseja editar?')
+        serie.buscaTudo()
+        index = int(input()) - 1
+        print(f'O que você deseja editar em {listaSeries[index]["titulo"]}?')
+        for i, k in enumerate(listaSeries[index].keys()):
+            if i > 1:
+                print(f'[{i - 1}] {k.capitalize()}')
+        key = int(input()) + 1
+        key = list(listaSeries[index].keys())[key]
+        print(f'Você deseja alterar {key.capitalize()}: {listaSeries[index][key]} para qual valor?')
+        valor = input()
+        serie.atualizar(index, key, valor)
+    elif r == 4:
+        print('Qual série deseja deletar?')
+        serie.buscaTudo()
+        index = int(input()) - 1
+        serie.deletar(index)
+    else:
+        break
 
-serie = Serie('visual', 1, 'Dark', 'Não sei', 2019, 'Suspense', 'Muito Bom!', 20, 'Também não sei')
-print(vars(serie))
+print('FIM')
